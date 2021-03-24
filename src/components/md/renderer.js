669980969d96de0d78d566581e21ecc395dcc0cd
ClarityAcuity <zonapha770709@hotmail.com>
2021-03-25 01:46:59 +0800

    Fix use static query

--- src/components/md/renderer.js
+++ src/components/md/renderer.js
@@ -1,53 +1,59 @@
 import React from "react"
 import { MDXRenderer } from "gatsby-plugin-mdx"
-import { useStaticQuery, graphql } from "gatsby"
+import { StaticQuery, graphql } from "gatsby"
 import SEO from "../seo"
 
 export default function MyPageLayout() {
-  const query = useStaticQuery(graphql`
-    query MDXQuery($id: String!) {
-      site {
-        siteMetadata {
-          title
-          author
-        }
-      }
-      mdx(id: { eq: $id }) {
-        id
-        body
-        slug
-        timeToRead
-        excerpt(pruneLength: 160)
-        frontmatter {
-          title
-          date
-          author
-          image: featured {
-            childImageSharp {
-              resize(width: 1200) {
-                src
-                height
-                width
+  return (
+    <StaticQuery
+      query={graphql`
+        query MDXQuery($id: String!) {
+          site {
+            siteMetadata {
+              title
+              author
+            }
+          }
+          mdx(id: { eq: $id }) {
+            id
+            body
+            slug
+            timeToRead
+            excerpt(pruneLength: 160)
+            frontmatter {
+              title
+              date
+              author
+              image: featured {
+                childImageSharp {
+                  resize(width: 1200) {
+                    src
+                    height
+                    width
+                  }
+                }
               }
             }
           }
         }
-      }
-    }
-  `)
-  const post = props.data
-  const image = post.frontmatter.image
-    ? post.frontmatter.image.childImageSharp.resize
-    : null
-  return (
-    <MDXRenderer>
-      <SEO
-        title={post.frontmatter.title}
-        description={post.frontmatter.description || post.excerpt}
-        image={image}
-        pathname={this.props.location.pathname}
-      />
-      {this.props.data.mdx.body}
-    </MDXRenderer>
+      `}
+      render={data => {
+        const post = props.data
+        const image = post.frontmatter.image
+          ? post.frontmatter.image.childImageSharp.resize
+          : null
+        return (
+          <MDXRenderer>
+            <SEO
+              title={post.frontmatter.title}
+              description={post.frontmatter.description || post.excerpt}
+              image={image}
+              pathname={this.props.location.pathname}
+            />
+            {data.mdx.body}
+          </MDXRenderer>
+        )
+      }}
+    />
   )
 }
