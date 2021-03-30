1a14f24f85cfc87f1f06339b4fbe39a025a9a8ef
ClarityAcuity <zonapha770709@hotmail.com>
2021-03-30 23:23:18 +0800

    Add icons

--- src/icons/index.js
+++ src/icons/index.js
@@ -1,9 +1,19 @@
-import { GitHubIcon } from './github'
-import { InstagramIcon } from './instagram'
-import { OctIcon } from './oct'
+import { GitHubIcon } from "./github"
+import { LinkedinIcon } from "./linkedin"
+import { DevtoIcon } from "./devto"
+import { TwitterIcon } from "./twitter"
+import { FacebookIcon } from "./facebook"
+import { InstagramIcon } from "./instagram"
+import { GatsbyIcon} from './gatsby'
+import { OctIcon } from "./oct"
 
 export {
   GitHubIcon,
+  LinkedinIcon,
+  DevtoIcon,
+  TwitterIcon,
+  FacebookIcon,
   InstagramIcon,
+  GatsbyIcon,
   OctIcon,
 }
